a065efa084f3268272b24e80f563f858adabdaac
Obinna Franklin Duru <obinnafranklinduru@gmail.com>
2023-11-16 18:47:57 +0100

    Update paymentSplitter.controller.js

--- src/controllers/paymentSplitter.controller.js
+++ src/controllers/paymentSplitter.controller.js
@@ -1,6 +1,8 @@
 const httpStatus = require('http-status');
 const ErrorResponse = require('../utils/errorResponse');
 
+const SplitTypes = ['FLAT', 'PERCENTAGE', 'RATIO'];
+
 /**
  * Compute payment splitting based on the provided transaction details and split rules.
  * @param {Object} req - Express request object.
@@ -10,21 +12,23 @@ const ErrorResponse = require('../utils/errorResponse');
  */
 async function computePaymentSplitting(req, res, next) {
     try {
-        const { ID, Amount, Currency, CustomerEmail, SplitInfo } = req.body;
+        const { ID, Amount, SplitInfo } = req.body;
 
-        // Cache the order of split types
-        const order = ['FLAT', 'PERCENTAGE', 'RATIO'];
+        // Cache the order of split types and create a map for faster lookup
+        const order = SplitTypes;
+        const splitTypeIndexMap = Object.fromEntries(order.map((type, index) => [type, index]));
 
         // Sort SplitInfo based on precedence rules
-        const sortedSplitInfo = SplitInfo.sort((a, b) => order.indexOf(a.SplitType) - order.indexOf(b.SplitType));
+        const sortedSplitInfo = SplitInfo.sort((a, b) => splitTypeIndexMap[a.SplitType] - splitTypeIndexMap[b.SplitType]);
 
         let balance = Amount;
         const splitBreakdown = [];
         let totalRatio = 0;
 
         // Iterate through each split entity and calculate the split amount
-        for (const { SplitType, SplitValue, SplitEntityId } of sortedSplitInfo) {
+        for (const splitInfo of sortedSplitInfo) {
             let amount = 0;
+            const { SplitType, SplitValue, SplitEntityId } = splitInfo;
 
             switch (SplitType) {
                 case 'FLAT':
@@ -35,12 +39,17 @@ async function computePaymentSplitting(req, res, next) {
                     break;
                 case 'RATIO':
                     if (totalRatio === 0) {
-                        totalRatio = sortedSplitInfo.filter((info) => info.SplitType === 'RATIO').reduce((acc, curr) => acc + curr.SplitValue, 0);
+                        for (const curr of sortedSplitInfo) {
+                            if (curr.SplitType === 'RATIO') {
+                                totalRatio += curr.SplitValue;
+                            }
+                        }
                     }
                     amount = Math.min((SplitValue / totalRatio) * balance, balance);
                     break;
                 default:
                     next(new ErrorResponse('Invalid SplitType', 400));
+                    return;
             }
 
             // Record the split breakdown
@@ -48,21 +57,26 @@ async function computePaymentSplitting(req, res, next) {
 
             // Update the balance for the next iteration
             balance -= amount;
+
+            // Exit early when balance becomes 0
+            if (balance === 0) {
+                break;
+            }
         }
 
         // Check constraints to ensure the validity of the computation
         if (balance < 0 || splitBreakdown.some((item) => item.Amount < 0) || splitBreakdown.reduce((acc, curr) => acc + curr.Amount, 0) > Amount) {
             next(new ErrorResponse('Invalid computation. Check constraints.', 400));
+            return;
         }
 
         // Respond with the computed results
         res.status(httpStatus.OK).json({
             ID,
             Balance: balance,
-            SplitBreakdown: splitBreakdown
+            SplitBreakdown: splitBreakdown,
         });
     } catch (error) {
-        // Pass any errors to the error-handling middleware
         next(error);
     }
 }
